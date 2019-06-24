8718c39b76983dea9e575be88f089b1477efdd4e
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-06-24 13:20:15 +0900

    add ListView

---
+++ delyClient/delyClient/View/FavoriteList.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteList.swift
+//  delyClient
+//
+//  Created by 小幡 十矛 on 2019/06/24.
+//  Copyright © 2019 Tomu Obata. All rights reserved.
+//
+
+import SwiftUI
+
+struct FavoriteList : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct FavoriteList_Previews : PreviewProvider {
+    static var previews: some View {
+        FavoriteList()
+    }
+}
+#endif
---
+++ delyClient/delyClient/View/RecipeList.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeList.swift
+//  delyClient
+//
+//  Created by 小幡 十矛 on 2019/06/24.
+//  Copyright © 2019 Tomu Obata. All rights reserved.
+//
+
+import SwiftUI
+
+struct RecipeList : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct RecipeList_Previews : PreviewProvider {
+    static var previews: some View {
+        RecipeList()
+    }
+}
+#endif

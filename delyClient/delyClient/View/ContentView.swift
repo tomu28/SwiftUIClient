56476da91e87781025751e266a47c7f82446bda1
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-06-24 18:31:04 +0900

    add favorite Botton

--- delyClient/delyClient/View/ContentView.swift
+++ delyClient/delyClient/View/ContentView.swift
@@ -12,11 +12,13 @@ struct ContentView : View {
     
     var body: some View {
         NavigationView{
+
             List() {
                 ForEach(0..<10) { index in
                     HStack {
                         ForEach(0..<1) { _ in
                             VStack {
+
                                 Image(uiImage: UIImage(url:
                                     APIDataList[index * 2]["image"]!!))
                                     .resizable()
@@ -24,6 +26,17 @@ struct ContentView : View {
                                     .padding(.bottom, 8.0)
                                     .padding(.trailing, 2.0)
                                     .padding(.leading, -6.0)
+                                    .overlay(
+                                        HStack {
+                                            Button(action:{}){
+                                                Image(systemName: "heart")
+                                            }
+                                        }
+                                        .padding()
+                                        .foregroundColor(.pink)
+                                        .background(Color.white.opacity(0.1)),
+                                        alignment: .topTrailing
+                                    )
                                 Text(APIDataList[index * 2]["title"]!!)
                                     .font(.system(size: 11))
                                     .fontWeight(.bold)
@@ -37,6 +50,17 @@ struct ContentView : View {
                                     .padding(.bottom, 8.0)
                                     .padding(.trailing, -6.0)
                                     .padding(.leading, 2.0)
+                                    .overlay(
+                                        HStack {
+                                            Button(action:{}){
+                                                Image(systemName: "heart")
+                                            }
+                                            }
+                                            .padding()
+                                            .foregroundColor(.pink)
+                                            .background(Color.white.opacity(0.1)),
+                                        alignment: .topTrailing
+                                    )
                                 Text(APIDataList[index * 2 + 1]["title"]!!)
                                     .font(.system(size: 11))
                                     .fontWeight(.bold)
@@ -49,6 +73,23 @@ struct ContentView : View {
             .foregroundColor(.orange)
             .navigationBarTitle(Text("料理"))
         }
+        
+//        Image("SwiftUI")
+//            .overlay(
+//                HStack {
+//                    Button(action:{}){
+//                        Image(systemName: "heart")
+//                    }
+//                    // Text("Artichokes") // Text to use as a caption.
+////                    Spacer()
+//                    }
+//                    .padding()
+//                    .foregroundColor(.white)
+//                    .background(Color.white.opacity(0.1)),
+//
+//                alignment: .topTrailing
+//        )
+    
     }
 
 }

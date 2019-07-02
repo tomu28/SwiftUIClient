a33d0d63a09f897b344ac44613cd6d1295d2c037
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2019-07-02 22:25:44 +0900

    add FavoriteList

--- delyClient/delyClient/View/FavoriteList.swift
+++ delyClient/delyClient/View/FavoriteList.swift
@@ -9,14 +9,60 @@
 import SwiftUI
 
 struct FavoriteList : View {
+    @State var ButtonStateList: [Int] = ButtonState
+
     var body: some View {
-        Text("FavoriteList")
-        
-        
-        // ButtonState.count で個数をカウント
-        // ex) [10, 8]
-        
-        
+        NavigationView{
+            // ButtonStateList = ButtonState
+            
+            List() {
+                ForEach(0..<ButtonState.count/2) { index in
+                    HStack {
+                        ForEach(0..<1) { _ in
+                            VStack {
+                                Image(uiImage: UIImage(url:
+                                    APIDataList[ButtonState[index * 2]]["image"]!!))
+                                    .resizable()
+                                    .scaledToFit()
+                                    .padding(.bottom, 8.0)
+                                    .padding(.trailing, 2.0)
+                                    .padding(.leading, -6.0)
+//                                    .tapAction
+//                                    {
+//                                        print(index * 2)
+//                                        print(self.ButtonStateList)
+//                                        print(self.ButtonStateList.count/2)
+//                                    }
+                                
+                                Text(APIDataList[ButtonState[index * 2]]["title"]!!)
+                                    .font(.system(size: 11))
+                                    .fontWeight(.bold)
+                                    .lineLimit(2)
+                            }
+//                            if index != ButtonState.count + 2 {
+                            VStack {
+                                Image(uiImage: UIImage(url:
+                                    APIDataList[ButtonState[index * 2 + 1]]["image"]!!))
+                                    .resizable()
+                                    .scaledToFit()
+                                    .padding(.bottom, 8.0)
+                                    .padding(.trailing, -6.0)
+                                    .padding(.leading, 2.0)
+                                
+                                Text(APIDataList[ButtonState[index * 2 + 1]]["title"]!!)
+                                    .font(.system(size: 11))
+                                    .fontWeight(.bold)
+                                    .lineLimit(2)
+                            }
+//                            }
+                        }
+                    }
+                }
+                }
+                .foregroundColor(.orange)
+                .navigationBarTitle(Text("お気に入り"))
+        }
+
     }
 }
 
